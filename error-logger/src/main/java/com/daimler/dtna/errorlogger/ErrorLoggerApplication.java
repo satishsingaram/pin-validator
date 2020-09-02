95dfa6f06c3b05b7c773be67d824541b34754316
satishsingaram <satish.singaram@gmail.com>
2020-09-02 23:29:19 +0530

    Added Eureka back

--- error-logger/src/main/java/com/daimler/dtna/errorlogger/ErrorLoggerApplication.java
+++ error-logger/src/main/java/com/daimler/dtna/errorlogger/ErrorLoggerApplication.java
@@ -2,6 +2,7 @@ package com.daimler.dtna.errorlogger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
